100328d68eb4a0b2bba5fcc6b1667936e73371c5
Ujjal Sigdel <081bei012.ujjal@sagarmatha.edu.np>
2025-03-15 16:18:40 +0545

    Sending emails

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import dotenv from "dotenv"; // <-- Added this line
+dotenv.config();        // <-- Added this line
 
 const app = express();
 app.use(express.json());
